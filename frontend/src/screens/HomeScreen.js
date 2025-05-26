548ffd3991fbe27bf81299b9c8ad121c09509fef
Katkuri Bharath Kumar <katkuribharathkumar04@gmail.com>
2025-05-26 17:19:31 +0530

    Update HomeScreen.js
    safely check products array before mapping

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -57,34 +57,34 @@ function HomeScreen(props) {
       ) : error ? (
         <div>{error}</div>
       ) : (
-        <ul className="products">
-          {products.map((product) => (
-            <li key={product._id}>
-              <div className="product">
-                <Link to={'/product/' + product._id}>
-                  <img
-                    className="product-image"
-                    src={product.image}
-                    alt="product"
-                  />
-                </Link>
-                <div className="product-name">
-                  <Link to={'/product/' + product._id}>{product.name}</Link>
-                </div>
-                <div className="product-brand">{product.brand}</div>
-                <div className="product-price">${product.price}</div>
-                <div className="product-rating">
-                  <Rating
-                    value={product.rating}
-                    text={product.numReviews + ' reviews'}
-                  />
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-    </>
-  );
-}
+       <ul className="products">
+  {Array.isArray(products) ? (
+    products.map((product) => (
+      <li key={product._id}>
+        <div className="product">
+          <Link to={'/product/' + product._id}>
+            <img
+              className="product-image"
+              src={product.image}
+              alt="product"
+            />
+          </Link>
+          <div className="product-name">
+            <Link to={'/product/' + product._id}>{product.name}</Link>
+          </div>
+          <div className="product-brand">{product.brand}</div>
+          <div className="product-price">${product.price}</div>
+          <div className="product-rating">
+            <Rating
+              value={product.rating}
+              text={product.numReviews + ' reviews'}
+            />
+          </div>
+        </div>
+      </li>
+    ))
+  ) : (
+    <li>No products found.</li>
+  )}
+</ul>
 export default HomeScreen;
