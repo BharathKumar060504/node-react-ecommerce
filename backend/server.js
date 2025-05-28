fadbbae95e05dadf7b6e6d976dd244ba6ef26046
Katkuri Bharath Kumar <katkuribharathkumar04@gmail.com>
2025-05-28 13:04:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
 
-app.listen(config.PORT, () => {
+app.listen(config.PORT, '0.0.0.0', () => {
   console.log('Server started at http://localhost:5000');
 });
